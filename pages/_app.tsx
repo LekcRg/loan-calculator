eb3d19d1c624c97818a23b7f9e843bcf4961d81f
Alexey Romanov <lekcrg@ya.ru>
2023-05-06 20:24:14 +0400

    Setting up a project. Docker, styled-components, fonts etc.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
+import localFont from 'next/font/local'
 import type { AppProps } from 'next/app'
 
+import '@/styles/globals.css'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component 
+    {...pageProps} 
+  />
 }
